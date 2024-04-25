7b7e0b4159d1158007355e6a362865f99df34176
Nicolas Paz <nicolas.paz.sde@gmail.com>
2024-04-24 21:05:41 -0300

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Amplify } from "aws-amplify";
+import amplifyconfig from './amplifyconfiguration.json';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Amplify.configure(amplifyconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
